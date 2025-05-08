8d56617ba15b2ff12529d8762c5fe41a812c321a
minpark <minje.park1@student.unimelb.edu.au>
2025-05-09 01:21:21 +1000

    First move-register

---
+++ frontend/src/app/Register/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./register.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.registerClass}>
+        <p>register</p>
+        <p>AI TaskPilot</p>
+    </div>
+  );
+}
