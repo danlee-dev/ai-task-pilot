d3330405ee4d4e92f2be6c21b334beb1bac8b3c6
hwoo317 <hwoo317k@gmail.com>
2025-05-09 01:21:45 +1000

    First move-login

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./login.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.loginClass}>
+      <p>Algorithm is fun!</p>
+      <p>Move on to login page!</p>
+          </div>
+  );
+}
